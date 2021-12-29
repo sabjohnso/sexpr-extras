3b9b0a0eba5d09a442ca5721d89b2fe952de6f5e
Samuel B. Johnson <sabjohnso@yahoo.com>
2021-12-29 16:06:25 -0600

    Fixed name for scribble file in info.rkt

--- info.rkt
+++ info.rkt
@@ -2,6 +2,6 @@
 (define collection "sexpr-extras")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/sexp-extras.scrbl" ())))
+(define scribblings '(("scribblings/sexpr-extras.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
